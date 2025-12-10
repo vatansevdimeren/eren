fa5ae6da3b04b292db7706380b1015cae096e07f
vatansevdimeren <vatansevdimeren@gmail.com>
2025-12-10 17:17:33 +0300

    Area

---
+++ calculating the area.c
@@ -0,0 +1,20 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#define PI 3.14
+
+int main() {
+	float  radius = 0;
+	double area = 0;
+	double circumference = 0;
+	
+	printf("PLEASE ENTER THE RADIUS OF THE CIRCLE:");
+	scanf_s("%f", &radius);
+
+	area = PI * radius * radius;
+	circumference = 2 * PI * radius;
+
+	printf("THE ARE OF THE CIRCLE IS :%.3f\n",area);
+	printf("THE CIRFUMFERENCE OF THE CIRCLE IS :%.3f\n", circumference);
+
+	return 0;
+}

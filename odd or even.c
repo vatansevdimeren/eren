bcd5e368a06ac263b58fadf3fc0d8161a3629a54
vatansevdimeren <vatansevdimeren@gmail.com>
2025-12-10 17:24:28 +0300

    odd or even

---
+++ odd or even.c
@@ -0,0 +1,17 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+int main() {
+	int number = 0;
+	
+	printf("PLEASE ENTER A NUMBER WHICH YOU WANT TO FIND OUT IF IT IS ODD OR EVEN:");
+	scanf_s("%d", &number);
+
+	if (number % 2 == 0) {
+		printf("THE NUMBER %d MUST BE EVEN!\n",number);
+	}
+	else {
+		printf("THE NUMBER %d MUST BE ODD!\n",number);
+	}
+
+	return 0;
+}

955de56117887bc49bb7904fdff8400f25721076
vatansevdimeren <vatansevdimeren@gmail.com>
2025-12-12 23:42:00 +0300

    creating strlen function by usin pointers

---
+++ creating a strlen function by using pointers.c
@@ -0,0 +1,16 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+int main() {
+	char array[50] = "IKNOWYOUWHATCHMERIDVAN";
+	char* ptri;
+	ptri = array;
+	int counter = 0;
+	while (*ptri != '\0') {
+		counter++;
+		ptri++;
+	}
+	printf("%d", counter);
+
+
+	return 0;
+}

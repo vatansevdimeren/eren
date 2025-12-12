3cbc39204c4a6ec701474a8078f1870fa609492e
vatansevdimeren <vatansevdimeren@gmail.com>
2025-12-12 12:44:59 +0300

    drawing pyramid by using loops

---
+++ drawing pyramid.c
@@ -0,0 +1,21 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+int main() {
+	int high = 0;
+
+	printf("PLEASE ENTER THE SIZE OF HIGH OF THE PYRAMID:");
+	scanf_s("%d", &high);
+
+	for (int i = 0; high > i; i++) {
+		for (int j = 0; high - 1 -i > j; j++) {
+			printf(" ");
+		}
+		for (int p = 0; p < ((2 * i) +1); p++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	system("pause");
+
+	return 0;
+}

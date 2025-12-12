769c46d878b73f74edd2f5b5e9803e09f5cedee3
vatansevdimeren <vatansevdimeren@gmail.com>
2025-12-12 13:11:19 +0300

    reverse array basic -1

---
+++ reverse array.c
@@ -0,0 +1,33 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+int main() {
+	int array[10];
+	int reversed_array[10];
+	printf("TO QIUT PRESS -1\n");
+	for (int i = 0; 10 > i; i++) {
+		array[i] = 0;
+		reversed_array[i] = 0;
+	}
+	for (int j = 0; 10 > j; j++) {
+		printf("PLEASE ENTER ANY NUMBER:");
+		
+		scanf_s("%d", &array[j]);
+		if (array[j] == -1) break;
+
+	}
+	
+	for (int p = 0; 10 > p; p++) {
+		reversed_array[9 - p] = array[p];
+	}
+	for (int k = 0; 10 > k; k++) {
+
+		if (reversed_array[k] != 0 && reversed_array[k]!=-1) {
+			printf("REVERSED ARRAY %d.=%d\n", k + 1, reversed_array[k]);
+		}
+		
+
+	}
+	system("pause");
+
+	return 0;
+}

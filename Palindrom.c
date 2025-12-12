aca30493b492f59e96270e78f1f0e0d7fd759018
vatansevdimeren <vatansevdimeren@gmail.com>
2025-12-12 13:41:40 +0300

    palindrom

---
+++ Palindrom.c
@@ -0,0 +1,33 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>//we will use the library to find the length of the world whıch user entered.
+int main() {
+	char world[25];
+	int checker = 0;//we need to use flag to determine if it is palindromic or not.
+	
+	printf("PLEASE ENTER A WORLD:");
+	scanf_s("%s", world, 25);//here,we dont have to use & to get string from by the user.
+
+	int length = 0;
+	length = strlen(world);
+	for (int i = 0; length / 2 > i; i++) {
+	
+		if (world[i] != world[length - 1 - i]) {
+			checker++;
+		}
+
+
+
+	}
+	
+	if (checker != 0) {
+		printf("IT IS NOT A POLINDROMIC STRING!\n");
+	}
+	else {
+		printf("IT IS A POLINDROMIC!\n");
+	}
+
+
+
+	return 0;
+}

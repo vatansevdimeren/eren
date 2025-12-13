7a4ba6797998c3a59eace122043f56ccc0d42b65
vatansevdimeren <vatansevdimeren@gmail.com>
2025-12-13 21:37:12 +0300

    encyrpto basic example -1-

---
+++ encrypto exampe -1-.c
@@ -0,0 +1,25 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+int main() {
+	char message[100];
+	int key = 0;
+	
+	printf("PLEASE ENTER WHAT DO YOU WANNA ENCRYPTO:");
+	scanf_s("%s", message,100);
+
+	printf("PLEASE ENTER THE VALUE OF KEY:");
+	scanf_s("%d", &key);
+
+	for (int i = 0; 100 > i; i++) {
+		if (message[i] != '\0') {
+			message[i] = message[i] + key;
+		}
+		else {
+			break;
+		}
+	}
+	printf("NEW PASSWORD IS %s", message);
+
+
+	return 0;
+}
